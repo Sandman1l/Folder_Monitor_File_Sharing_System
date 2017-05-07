46e715a1da3eebfbe96eb24e346eece730d4ca5b
LuisSFU <Saloch_1s@hotmail.com>
2017-05-06 20:24:34 -0500

    uncredit

--- ServerTCP.c
+++ ServerTCP.c
@@ -1,96 +1,113 @@
-
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <errno.h>
 #include <string.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <sys/types.h>
 #include <netinet/in.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <signal.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#define PORT_NUMBER 5000
-#define SERVER_ADDRESS  "10.15.127.52"
-#define FILE_NAME  "hello.c"
+#define PORT 20000
+#define BACKLOG 5
 #define LENGTH 512
-#define FOLDER_EXTENSION "/Users/LuisSFU/Desktop/Mission7/"
 
-int main(int argc, char **argv)
+void error(const char *msg)
 {
-        int server_socket;
-        int peer_socket;
-        struct sockaddr_in server_addr;
-        struct sockaddr_in peer_addr;
-        struct stat file_stat;
-        socklen_t socket_size;
-        int socket_fd;
-        char buffer[LENGTH];
-        //Create server socket
-        server_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (server_socket == -1)
-        {
-                fprintf(stderr, "Error: Failed to create socket.");
+	perror(msg);
+	exit(1);
+}
 
-                exit(EXIT_FAILURE);
-        }
-        /* Zeroing server_addr struct */
-        memset(&server_addr, 0, sizeof(server_addr));
-        /* Construct server_addr struct */
-        server_addr.sin_family = AF_INET;
-        inet_pton(AF_INET, SERVER_ADDRESS, &(server_addr.sin_addr));
-        server_addr.sin_port = htons(PORT_NUMBER);
+int main ()
+{
+	/* Defining Variables */
+	int sockfd;
+	int nsockfd;
+	int num;
+	socklen_t sin_size;
+	struct sockaddr_in addr_local; /* client addr */
+	struct sockaddr_in addr_remote; /* server addr */
+	char revbuf[LENGTH]; // Receiver buffer
 
-        /* Bind */
-        if ((bind(server_socket, (struct sockaddr *)&server_addr, sizeof(struct sockaddr))) == -1)
-        {
-                fprintf(stderr, "Error: Failed to bind.");
+	/* Get the Socket file descriptor */
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 )
+	{
+	    error("ERROR: Failed to obtain Socket Descriptor.\n");
+	}
+	else
+		printf("[Server] Obtaining socket descriptor successfully.\n");
 
-                exit(EXIT_FAILURE);
-        }
+	/* Fill the client socket address struct */
+	addr_local.sin_family = AF_INET; // Protocol Family
+	addr_local.sin_port = htons(PORT); // Port number
+	addr_local.sin_addr.s_addr = INADDR_ANY; // AutoFill local address
+	bzero(&(addr_local.sin_zero), 8); // Flush the rest of struct
 
-        /* Listening to incoming connections */
-        if(listen(server_socket,5) == -1){
-	         printf("Error: Failed to listen Port.\n");
-           exit(EXIT_FAILURE);
-	        }else{
-		          printf ("SUCCESS: Listening the port %d.\n", PORT_NUMBER);
-            }
-	           int success = 0;
-	           while(success == 0){
-	            socket_size = sizeof(struct sockaddr_in);
-	           //Wait a connection
-	            if ((socket_fd = accept(server_socket, (struct sockaddr *)&server_addr, &socket_size)) == -1){
-	                printf("Error:  Failed to Obtain new Socket Despcritor.\n");
-		            }else{
-			               printf("NOTICE: Server has got connected from %s.\n", inet_ntoa(server_addr.sin_addr));
-                   }
-		               //Recieve file
-		               char* file_name = "/Users/LuisSFU/Desktop/Mission7/foo.c";
-		               FILE *file = fopen(file_name, "a");
-		               if(file == NULL){
-			             printf("File %s Cannot be opened file.\n", file_name);
-                    }else{
-			                bzero(buffer, LENGTH);
-                    }
-			            int blockSize = 0;
-                  int writeSize;
-			            while((blockSize = recv(server_socket, buffer, LENGTH, 0))){
-			                 if(blockSize < 0){
-			                   printf("Error receiving file.\n");
-			                 }
-			                   writeSize = fwrite(buffer, sizeof(char), blockSize, file);
-			                   if(writeSize < blockSize){
-			                       printf("File write failed.\n");
-                           }else if(blockSize){
-						                    break;
-                              }
-  			       bzero(buffer, LENGTH);
+	/* Bind a special Port */
+	if( bind(sockfd, (struct sockaddr*)&addr_local, sizeof(struct sockaddr)) == -1 )
+	{
+	    error("ERROR: Failed to bind Port.\n");
+	}
+	else
+		printf("[Server] Binded tcp port %d sucessfully.\n",PORT);
+
+	/* Listen remote connect/calling */
+	if(listen(sockfd,BACKLOG) == -1)
+	{
+	    error("ERROR: Failed to listen Port.\n");
+	}
+	else
+		printf ("[Server] Listening the port %d successfully.\n", PORT);
+
+	int success = 0;
+	while(success == 0)
+	{
+	    sin_size = sizeof(struct sockaddr_in);
+
+	    /* Wait a connection, and obtain a new socket file despriptor for single connection */
+	    if ((nsockfd = accept(sockfd, (struct sockaddr *)&addr_remote, &sin_size)) == -1)
+		{
+	        error("ERROR: Obtaining new Socket Despcritor.\n");
+		}
+	    else
+			printf("[Server] Server has got connected from %s.\n", inet_ntoa(addr_remote.sin_addr));
+
+		/*Receive File from Client */
+		char* fr_name = "/Users/LuisSFU/Desktop/Mission7/foo.c";
+		FILE *fr = fopen(fr_name, "a");
+		if(fr == NULL)
+			printf("File %s Cannot be opened file on server.\n", fr_name);
+		else
+		{
+			bzero(revbuf, LENGTH);
+			int fr_block_sz = 0;
+			//int success = 0;
+			//while(success == 0)
+			//{
+			    while((fr_block_sz = recv(nsockfd, revbuf, LENGTH, 0))) //could it be sockfd?
+			    {
+			        if(fr_block_sz < 0)
+			        {
+			            error("Error receiving file from client to server.\n");
+			        }
+			        int write_sz = fwrite(revbuf, sizeof(char), fr_block_sz, fr);
+			        if(write_sz < fr_block_sz)
+			        {
+			            error("File write failed on server.\n");
+			        }
+					else if(fr_block_sz)
+					{
+						break;
+					}
+			        bzero(revbuf, LENGTH);
 			    }
-			    printf("Ok received new File!\n");
-			    fclose(file);
+			    printf("Ok received from client!\n");
+			    fclose(fr);
 			//}
 		}
-
+return 0;
 }
+	}
